df8b6ff2f90748570f7efb234e92071b22558e31
“margaritisarg” <margaritis1argyrou@gmail.com>
2023-04-21 10:29:30 +0100

    css changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import navigationCSS from './css/navigation.module.css'
 
 const Nav = () =>{
+
+    const [order, setOrder] = useState('desc')
+
+    function onChangeOrder(event){
+        setOrder(event.target.value)
+    }
+
     return(
-        <div className={navigationCSS.navigation}>
+        <>
+        <div className={`${navigationCSS.navigation} ${navigationCSS.grid4}`}>
             <Link to="/" className={navigationCSS.link}>All</Link>
-            <Link to="/reviews/hidden-roles" className={navigationCSS.link}>hidden-roles</Link>
+            <Link to={`/reviews/hidden-roles`} className={navigationCSS.link}>hidden-roles</Link>
             <Link to="/reviews/dexterity" className={navigationCSS.link}>dexterity</Link>
             <Link to="/reviews/strategy" className={navigationCSS.link}>strategy</Link>
             <Link to="/reviews/push-your-luck" className={navigationCSS.link}>push-your-luck</Link>
@@ -13,6 +22,16 @@ const Nav = () =>{
             <Link to="/reviews/deck-building" className={navigationCSS.link}>deck-building</Link>
             <Link to="/reviews/engine-building" className={navigationCSS.link}>engine-building</Link>
         </div>
+        <div className={`${navigationCSS.navigation} ${navigationCSS.grid2}`}>
+            <select onChange={onChangeOrder} defaultValue={order}>
+                <option key="desc" value="desc">Descending</option>
+                <option key="asc" value="asc">Ascending</option>
+            </select>
+            <select>
+                <option value="anything">Anything</option>
+            </select>
+        </div>
+        </>
     )
 }
 
